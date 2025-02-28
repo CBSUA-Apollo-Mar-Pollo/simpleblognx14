9dbc2ca64ede417c7efebb852181ae92ec8fd9cd
Apollo Mar Pollo <103560480+CBSUA-Apollo-Mar-Pollo@users.noreply.github.com>
2025-02-28 21:06:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    //Only For Next.js versions prior to 14.1.0 because it is enabled by default since version 14.1.0
-    windowHistorySupport: true,
-     nextFont: false, 
-  },
+reactStrictMode: true,
   images: {
     remotePatterns: [
       {
