7a850a43960539c43ccbb110983ceb77e880e15e
Apollo Mar Pollo <103560480+CBSUA-Apollo-Mar-Pollo@users.noreply.github.com>
2025-02-28 20:45:02 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-poppins",
-  weight: "300",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export default function RootLayout({ children }) {
