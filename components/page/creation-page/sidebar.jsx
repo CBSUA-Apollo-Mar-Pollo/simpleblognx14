e463239fb11367282c47e512aee959953c170144
CBSUA-Apollo-Mar-Pollo <103560480+CBSUA-Apollo-Mar-Pollo@users.noreply.github.com>
2025-08-30 19:15:25 +0800

    update
    - added forms in side bar

--- components/page/creation-page/sidebar.jsx
+++ components/page/creation-page/sidebar.jsx
@@ -1,14 +1,46 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
 import { Separator } from "@/components/ui/Separator";
+import { Textarea } from "@/components/ui/Textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const PageCreationInputSchema = z.object({
+  pagename: z.string(),
+  // pagecategory: z.enum(["sports", "tech", "health", "music", "food"]),
+  // pagebio: z.string(),
+});
 
 const PageCreationSideBar = () => {
+  const form = useForm({
+    resolver: zodResolver(PageCreationInputSchema),
+    defaultValues: {
+      pagename: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data, "the page creation input values");
+  };
+
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <div className="flex items-center py-2 pl-3 gap-x-2">
         <Link href="/" className="p-2 bg-neutral-500 rounded-full">
           <X className="text-white" />
@@ -22,7 +54,7 @@ const PageCreationSideBar = () => {
 
       <Separator className="my-2" />
 
-      <div className="px-4 pt-3">
+      <div className="px-4 pt-3 flex-1 relative">
         <div className="flex items-center">
           <Link
             href="/pages"
@@ -40,6 +72,90 @@ const PageCreationSideBar = () => {
             This is your spotlight. Make sure it shines with all the info people
             need to connect with you.
           </p>
+
+          <div className="mt-4">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col space-y-3">
+                  <FormField
+                    control={form.control}
+                    name="pagename"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Page name(required)</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="h-14 border-neutral-300 rounded-2xl focus:border-2  focus:border-blue-600 "
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription className="text-[12px] text-neutral-700">
+                          Use the name of your business, brand, or organization,
+                          or choose a name that clearly represents the purpose
+                          of your Page."
+                        </FormDescription>
+
+                        <FormMessage className="text-[12px] ml-2" />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="pagecategory"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Category(required)</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="h-14 border-neutral-300  rounded-2xl focus:border-2  focus:border-blue-600 "
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription className="text-[12px] text-neutral-700">
+                          Your Page needs to have a category. Enter a category
+                          that best describes you.
+                        </FormDescription>
+
+                        <FormMessage className="text-[12px] ml-2" />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="pagebio"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Bio (optional)</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            className="border border-neutral-300 resize-none  rounded-2xl focus:border-2  focus:border-blue-600 p-4"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription className="text-[12px] text-neutral-700">
+                          Tell people a little about what you do.
+                        </FormDescription>
+
+                        <FormMessage className="text-[12px] ml-2" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="absolute bottom-0 left-0 w-full px-3">
+                  <Button disabled={true} type="submit" className="w-full">
+                    Create page
+                  </Button>
+
+                  <p className="text-xs text-center text-neutral-700 mt-1">
+                    By creating a Page, you agree to the Pages, Groups and
+                    Events Policies
+                  </p>
+                </div>
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
